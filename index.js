421c967914b80f40c2aab6bd5f5f8076889d97a1
GauravTheBeginner <gaurav8861r@gmail.com>
2024-03-25 15:36:20 +0530

    changes commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mainRouter = require("../src/routes/index")
+const mainRouter = require("./src/routes/index")
 const app = express();
 const cors = require('cors')
 
